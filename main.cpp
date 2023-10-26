2fe1331aee0ce1f67c53d8b11c6f3868bf0d62af
maksim.semenov <maksim.semenov@jetbrains.com>
2023-10-26 13:15:45 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+#ifdef _WIN32
+const std::string OS = "Windows";
+#elif _APPLE_
+const std::string OS = "macOS";
+#elif _linux
+const std::string OS = "Linux";
+#else
+const std::string OS = "unknown";
+#endif
+
+int main() {
+    std::cout << "Hello world, I'm running on " << OS << std::endl;
+    return 0;
+}
